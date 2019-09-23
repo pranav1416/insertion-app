d0d2dc8c75bc1b574f405ba352fd4f67bde485f8
Pranav Borole <axel@Pranavs-MBP.lan1>
2019-09-23 15:49:58 -0700

    Finalized First Iteration

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { StringifyOptions } from 'querystring';
-import { staticViewQueryIds } from '@angular/compiler';
-import { VirtualTimeScheduler } from 'rxjs';
+import { ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,43 +8,72 @@ import { VirtualTimeScheduler } from 'rxjs';
 })
 export class HomePage {
 
-  constructor() {}
+  unsortedSeq: any
+  isAlphabet: boolean
   htmlString: string = "<p></p>"
-  addText(textValue: string, iteration: number){
-    if(iteration==1){
-      this.htmlString = "<p>The intermediate Steps:</p>"  
+  btnValue: boolean = true
+  ipVal: string
+
+  constructor(public toastController: ToastController) { }
+
+  addText(textValue: string, iteration: number) {
+    if (iteration == 1) {
+      this.htmlString = "<p>The intermediate Steps:</p>"
     }
-    this.htmlString += "<p>Iteration " + iteration + ": " + textValue + " </p>" 
+    this.htmlString += "<p>Iteration " + iteration + ": " + textValue + " </p>"
   }
 
-  sortSeq(inputValue: string){
-    var unsortedSeq= inputValue.split(',').map(Number);
-    if(unsortedSeq.length != 9){
-      window.alert("Please enter valid Input!")
+  btnDisabled(inputValue: string){
+    var unsortedSeq= inputValue.split(',')
+    if(unsortedSeq.length == 9 && Number.isInteger(parseInt(unsortedSeq[8]))){
+      this.btnValue = false
     }
-    else{
+    else {
+      this.btnValue = true
+    } 
+  }
+
+  sortSeq(inputValue: string) {
+    this.unsortedSeq = inputValue.split(',').map(Number)
+    this.isAlphabet = false
+    for (var i in this.unsortedSeq) {
+      var seq = this.unsortedSeq[i]
+      if(isNaN(seq)){
+        this.isAlphabet = true
+        this.validationToast()
+      }
+    }
+    if (this.unsortedSeq.length != 9 || this.isAlphabet == true) {
+      this.validationToast()
+    }
+    else {
       var sortedSeq: number[];
-      var sortedSeq= this.insertionSort(unsortedSeq);
-      this.htmlString += "<p> The Sorted Sequence is : "+ sortedSeq + "</p>"
+      var sortedSeq = this.insertionSort(this.unsortedSeq)
+      this.htmlString += "<p> The Sorted Sequence is : " + sortedSeq + "</p>"
     }
   }
 
   insertionSort(inputSeq: number[]) {
-    var seq:number[] = inputSeq;
-    var indices: number[] = Array.from(Array(inputSeq.length).keys());
+    var seq: number[] = inputSeq;
 
-    for(var i = 0; i < seq.length; i++) {
+    for (var i = 0; i < seq.length; i++) {
       var temp = seq[i];
       var j = i - 1;
       while (j >= 0 && seq[j] > temp) {
         seq[j + 1] = seq[j];
-        indices[j+1] = indices[j]
         j--;
       }
       seq[j + 1] = temp;
-      indices[j+1]=i;
-      this.addText(seq.toString(),i+1)
-     }
-    return seq;
+      this.addText(seq.toString(), i + 1)
     }
-  }  
+    return seq;
+  }
+
+  async validationToast() {
+    const toast = await this.toastController.create({
+      message: 'Please enter valid Input!',
+      duration: 2000
+    });
+    toast.present();
+  }
+}  
