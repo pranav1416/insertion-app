93009646a1eb31b32ccc365bdc0c1cd20bb23015
Pranav Borole <axel@Pranavs-MBP.lan1>
2019-09-23 01:22:47 -0700

    Added button disable function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,14 +10,25 @@ export class HomePage {
 
   unsortedSeq: any
   isAlphabet: boolean
+  htmlString: string = "<p></p>"
+  btnValue: boolean = true
 
   constructor(public toastController: ToastController) { }
-  htmlString: string = "<p></p>"
   addText(textValue: string, iteration: number) {
     if (iteration == 1) {
       this.htmlString = "<p>The intermediate Steps:</p>"
     }
     this.htmlString += "<p>Iteration " + iteration + ": " + textValue + " </p>"
+  }
+
+  btnDisabled(inputValue: string){
+    var unsortedSeq= inputValue.split(',');
+    if(unsortedSeq.length == 9 && Number.isInteger(parseInt(unsortedSeq[8]))){
+      this.btnValue = false
+    }
+    else {
+      this.btnValue = true
+    } 
   }
 
   sortSeq(inputValue: string) {
