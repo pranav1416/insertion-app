a3f0ac0c826037cdfc486a23a8d509a1f833ad7f
Pranav Borole <axel@Pranavs-MBP.lan1>
2019-09-23 00:53:55 -0700

    Added button disable functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { StringifyOptions } from 'querystring';
-import { staticViewQueryIds } from '@angular/compiler';
-import { VirtualTimeScheduler } from 'rxjs';
+import { ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,7 +8,8 @@ import { VirtualTimeScheduler } from 'rxjs';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public toastController: ToastController) {}
+  btnValue: boolean = true;
   htmlString: string = "<p></p>"
   addText(textValue: string, iteration: number){
     if(iteration==1){
@@ -17,35 +17,42 @@ export class HomePage {
     }
     this.htmlString += "<p>Iteration " + iteration + ": " + textValue + " </p>" 
   }
+  btnDisabled(inputValue: string){
+    var unsortedSeq= inputValue.split(',');
+    if(unsortedSeq.length == 9 && Number.isInteger(parseInt(unsortedSeq[8]))){
+      this.btnValue = false
+    }
+    else {
+      this.btnValue = true
+    } 
+  }
 
   sortSeq(inputValue: string){
     var unsortedSeq= inputValue.split(',').map(Number);
-    if(unsortedSeq.length != 9){
-      window.alert("Please enter valid Input!")
-    }
-    else{
-      var sortedSeq: number[];
-      var sortedSeq= this.insertionSort(unsortedSeq);
-      this.htmlString += "<p> The Sorted Sequence is : "+ sortedSeq + "</p>"
-    }
+    var sortedSeq: number[] = this.insertionSort(unsortedSeq);
+    this.htmlString += "<p> The Sorted Sequence is : "+ sortedSeq + "</p>"
   }
 
-  insertionSort(inputSeq: number[]) {
-    var seq:number[] = inputSeq;
-    var indices: number[] = Array.from(Array(inputSeq.length).keys());
+  insertionSort(seq: number[]) {
 
     for(var i = 0; i < seq.length; i++) {
       var temp = seq[i];
       var j = i - 1;
       while (j >= 0 && seq[j] > temp) {
         seq[j + 1] = seq[j];
-        indices[j+1] = indices[j]
         j--;
       }
       seq[j + 1] = temp;
-      indices[j+1]=i;
       this.addText(seq.toString(),i+1)
      }
     return seq;
+    }
+
+    async validationToast() {
+      const toast = await this.toastController.create({
+        message: 'Please enter valid Input!',
+        duration: 2000
+      });
+      toast.present();
     }
   }  
