59666715f10098f4dfdcb2f362e9e1625673360c
Ankita Kulkarni <kulkarniankita555@gmail.com>
2019-09-22 17:36:18 -0700

    Added validation check for alphabet input

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,53 +8,62 @@ import { ToastController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(public toastController: ToastController) {}
+  unsortedSeq: any
+  isAlphabet: boolean
+
+  constructor(public toastController: ToastController) { }
   htmlString: string = "<p></p>"
-  addText(textValue: string, iteration: number){
-    if(iteration==1){
-      this.htmlString = "<p>The intermediate Steps:</p>"  
+  addText(textValue: string, iteration: number) {
+    if (iteration == 1) {
+      this.htmlString = "<p>The intermediate Steps:</p>"
     }
-    this.htmlString += "<p>Iteration " + iteration + ": " + textValue + " </p>" 
+    this.htmlString += "<p>Iteration " + iteration + ": " + textValue + " </p>"
   }
 
-  sortSeq(inputValue: string){
-    var unsortedSeq= inputValue.split(',').map(Number);
-    if(unsortedSeq.length != 9){
-      this.validationToast(unsortedSeq)
+  sortSeq(inputValue: string) {
+    this.unsortedSeq = inputValue.split(',').map(Number);
+    this.isAlphabet = false
+    for (var i in this.unsortedSeq) {
+      var seq = this.unsortedSeq[i]
+      if(isNaN(seq)){
+        this.isAlphabet = true
+        this.validationToast()
+      }
+      }
+    if (this.unsortedSeq.length != 9 || this.isAlphabet == true) {
+      this.validationToast()
     }
-    else{
+    else {
       var sortedSeq: number[];
-      var sortedSeq= this.insertionSort(unsortedSeq);
-      this.htmlString += "<p> The Sorted Sequence is : "+ sortedSeq + "</p>"
+      var sortedSeq = this.insertionSort(this.unsortedSeq);
+      this.htmlString += "<p> The Sorted Sequence is : " + sortedSeq + "</p>"
     }
   }
 
   insertionSort(inputSeq: number[]) {
-    var seq:number[] = inputSeq;
+    var seq: number[] = inputSeq;
     var indices: number[] = Array.from(Array(inputSeq.length).keys());
 
-    for(var i = 0; i < seq.length; i++) {
+    for (var i = 0; i < seq.length; i++) {
       var temp = seq[i];
       var j = i - 1;
       while (j >= 0 && seq[j] > temp) {
         seq[j + 1] = seq[j];
-        indices[j+1] = indices[j]
+        indices[j + 1] = indices[j]
         j--;
       }
       seq[j + 1] = temp;
-      indices[j+1]=i;
-      this.addText(seq.toString(),i+1)
-     }
+      indices[j + 1] = i;
+      this.addText(seq.toString(), i + 1)
+    }
     return seq;
-    }
+  }
 
-    async validationToast(unsortedSeq) {
-      if (unsortedSeq.length != 9) {
-        const toast = await this.toastController.create({
-          message: 'Please enter valid Input!',
-          duration: 2000
-        });
-        toast.present();
-      }
-    }
-  }  
+  async validationToast() {
+    const toast = await this.toastController.create({
+      message: 'Please enter valid Input!',
+      duration: 2000
+    });
+    toast.present();
+  }
+}  
